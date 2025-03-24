c969cfe36582352a8cb88651b8b11b3496fcf1fd
Zach Godsell <zach@zachgodsell.com>
2025-03-24 20:15:17 +1000

    WIP

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { type Metadata } from 'next'
 
 import { RootLayout } from '@/components/RootLayout'
+import { Analytics } from '@vercel/analytics/react'
 
 import '@/styles/tailwind.css'
 
@@ -16,6 +17,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <html lang="en" className="h-full bg-neutral-950 text-base antialiased">
       <body className="flex min-h-full flex-col">
         <RootLayout>{children}</RootLayout>
+        <Analytics />
       </body>
     </html>
   )
